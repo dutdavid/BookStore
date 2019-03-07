8de1b4a5df9f8c3a1b0beb6d1bcc6380fa7079c2
dutdavid <dutdavid7@gmail.com>
2019-03-07 14:07:31 +0300

    services

---
+++ app/src/main/java/com/moringa/bookstore/Constants.java
@@ -0,0 +1,11 @@
+package com.moringa.bookstore;
+
+
+public class Constants {
+    public static final String BASE_URL = "https://www.goodreads.com/search/index.xml";
+    public static final String API_PARAMETER = "key";
+    public static final String GOODREADS_API_KEY = BuildConfig.GOODREADS_API_KEY;
+    public static final String GOOD_READS_SECRET = BuildConfig.GOOD_READS_SECRET;
+    public static final String QUERY_PARAMETER = "q";
+
+}
---
+++ app/src/main/java/com/moringa/bookstore/GoodReadsServices.java
@@ -0,0 +1,4 @@
+package com.moringa.bookstore;
+
+public class GoodReadsServices {
+}
