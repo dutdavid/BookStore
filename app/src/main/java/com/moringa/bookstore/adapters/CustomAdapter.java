a0176539836f2899a2995419af3ad04f6f26d370
dutdavid <dutdavid7@gmail.com>
2019-03-09 10:24:15 +0300

    custom adapter

--- app/src/main/java/com/moringa/bookstore/adapters/CustomAdapter.java
+++ app/src/main/java/com/moringa/bookstore/adapters/CustomAdapter.java
@@ -1,4 +1,38 @@
 package com.moringa.bookstore.adapters;
 
-public class CustomAdapter {
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.moringa.bookstore.R;
+
+
+public class CustomAdapter extends ArrayAdapter {
+
+
+    public CustomAdapter(Context context, String[] books) {
+        super(context, R.layout.bookslist, books);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater booksInflater = LayoutInflater.from(getContext());
+        View customView = booksInflater.inflate(R.layout.bookslist, parent, false);
+
+        String bookItem = String.valueOf(getItem(position));
+        TextView author = (TextView) customView.findViewById(R.id.authorName);
+        ImageView imageBook = (ImageView) customView.findViewById(R.id.bookImage);
+
+        author.setText(bookItem);
+        imageBook.setImageResource(R.drawable.theshining);
+        return customView;
+    }
 }
