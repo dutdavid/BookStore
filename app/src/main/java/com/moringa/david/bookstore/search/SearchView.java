b204fd7149e470a56a514e04600f1312cfd2d4fb
dutdavid <dutdavid7@gmail.com>
2019-03-10 13:09:07 +0300

    background

--- app/src/main/java/com/moringa/david/bookstore/search/SearchView.java
+++ app/src/main/java/com/moringa/david/bookstore/search/SearchView.java
@@ -6,10 +6,7 @@ import com.moringa.david.bookstore.util.Toastable;
 
 interface SearchView extends Toastable {
 
-    /**
-     * Adds a book relevant to the search query
-     * @param book book object parsed from the response XML
-     */
+
     void showBookResult(Book book);
 
 }
