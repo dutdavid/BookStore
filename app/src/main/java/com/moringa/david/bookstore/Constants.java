bded4edc6026acd9862509ae1362a9ef8d55dfa7
dutdavid <dutdavid7@gmail.com>
2019-03-11 12:23:02 +0300

    firebase

---
+++ app/src/main/java/com/moringa/david/bookstore/Constants.java
@@ -0,0 +1,6 @@
+//package com.moringa.david.bookstore;
+//public class Constants {
+//    public static final String GR_API_Key = BuildConfig.GR_API_Key;
+//    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search";
+//    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";}
+//
